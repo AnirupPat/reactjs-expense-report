09038f086515a20fb68c860c595efe71b70960d2
Anirup Patnaik <anirup049@gmail.com>
2021-05-24 19:23:19 +0530

    ExpenseForm and NewExpense Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 import "./App.css";
 
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Lets get started</h2>
+      <NewExpense></NewExpense>
       <Expenses expenseData={expense}  />
     </div>
   );
