07785fb285cbabf39c1c63e49637a6f7e7187ef6
Anirup Patnaik <anirup049@gmail.com>
2021-05-24 22:39:50 +0530

    ExpenseFilter Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpresesFilter from './ExpensesFilter';
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021');
   let expense = props.expenseData;
+  const handleProcessSelectedYear = (selectedYear) => {
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  }
   return (
+    <div>
     <Card className="expenses">
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-    </Card>
+    <ExpresesFilter selected={filteredYear} getSelectedYear={handleProcessSelectedYear} />
+    <ExpenseItem
+      title={expense[0].title}
+      amount={expense[0].amount}
+      date={expense[0].date}
+    ></ExpenseItem>
+    <ExpenseItem
+      title={expense[1].title}
+      amount={expense[1].amount}
+      date={expense[1].date}
+    ></ExpenseItem>
+  </Card>
+    </div>
   );
 }
 
