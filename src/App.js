4bf926c0dcd3c3f8782a1d98201b2da079c636c3
Anirup Patnaik <anirup049@gmail.com>
2021-05-24 13:43:07 +0530

    Passing props to Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
+  let expense = [
+    {
+      title: "Car Insurance",
+      amount: 242.56,
+      date: new Date(2021, 5, 24),
+    },
+    {
+      title: "Bike Insurance",
+      amount: 142.56,
+      date: new Date(2021, 5, 23),
+    },
+    {
+      title: "Magazines",
+      amount: 24.0,
+      date: new Date(2021, 5, 22),
+    },
+    {
+      title: "Groceries",
+      amount: 100.0,
+      date: new Date(2021, 5, 21),
+    },
+  ];
   return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItem>
     </div>
   );
 }
